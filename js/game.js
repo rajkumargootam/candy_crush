a7c2df90b3afb650bc26de30441d9bae2a8db888
rajkumar <rajkumar.gootam@gmail.com>
2016-01-28 17:03:45 +0530

    Populated cells with coordinates

--- js/game.js
+++ js/game.js
@@ -1,14 +1,40 @@
 var Game;
 
 Game = {
-  shapes: ["heart", "star", "square", "circle", "rocket", "car"],
+  shapes: ["heart", "star", "square", "circle", "rocket", "hashtag"],
   randomShapesClass: function() {
     return "fa-" + Game.shapes[Math.floor(Math.random() * Game.shapes.length)];
   },
-  init: function() {
+  populateCellsWithShapes: function() {
     return $.each($(".cell i"), function(i, ele) {
       return $(ele).addClass(Game.randomShapesClass);
     });
+  },
+  populateCellCoordinates: function() {
+    var colNo, rowNo;
+    rowNo = 1;
+    colNo = 1;
+    $.each($("#board .row"), function(i, row) {
+      colNo = 1;
+      $.each($(row).children('.cell'), function(j, cell) {
+        cell.dataset.rowNo = rowNo;
+        cell.dataset.colNo = colNo;
+        return colNo++;
+      });
+      return rowNo++;
+    });
+    Game.rowsCount = rowNo;
+    return Game.columnsCount = colNo;
+  },
+  checkMatches: function() {
+    return console.log("checking matches");
+  },
+  init: function() {
+    Game.rowsCount = 0;
+    Game.columnsCount = 0;
+    Game.populateCellsWithShapes();
+    Game.populateCellCoordinates();
+    return Game.checkMatches();
   }
 };
 
--- js/src/game.coffee
+++ js/src/game.coffee
@@ -1,8 +1,29 @@
 Game =
-shapes: ["heart","star","square","circle","rocket","car"]
-randomShapesClass: ->
+  shapes: ["heart","star","square","circle","rocket","hashtag"]
+  randomShapesClass: ->
     "fa-" + Game.shapes[Math.floor(Math.random()*Game.shapes.length)]
-init: ->
-  $.each $(".cell i"), (i,ele) -> $(ele).addClass Game.randomShapesClass
+  populateCellsWithShapes: ->
+    $.each $(".cell i"), (i,ele) -> $(ele).addClass Game.randomShapesClass
+  populateCellCoordinates: ->
+    rowNo = 1
+    colNo = 1
+    $.each $("#board .row"), (i, row) ->
+      colNo = 1
+      $.each $(row).children('.cell'), (j, cell) ->
+        cell.dataset.rowNo = rowNo
+        cell.dataset.colNo = colNo
+        colNo++
+      rowNo++
+    Game.rowsCount = rowNo
+    Game.columnsCount = colNo
+  checkMatches: ->
+    console.log "checking matches"
+  init: ->
+    Game.rowsCount = 0
+    Game.columnsCount = 0
+    Game.populateCellsWithShapes()
+    Game.populateCellCoordinates()
+    Game.checkMatches()
+
 $ ->
    Game.init()
